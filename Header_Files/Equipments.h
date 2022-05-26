c03440c1427c2ebde54ca2a351ce45e39650aca1
Hassan Mahmood <85311173+HassanMahmood001@users.noreply.github.com>
2022-05-26 11:14:36 +0500

    Add files via upload

---
+++ Header_Files/Equipments.h
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <windows.h>
+using namespace std;
+void Equipments()
+{  
+  cout<<"Following are the equipment available in the GYM "<<endl;
+  cout<<"SQUAT RACK\nBARBELLS\nBENCH PRESS\nINCLINE BENCH PRESS\nHAMMER STRENGTH MACHINE\nCABLES AND PULLEYS\nDUMBBELLS\nPULLUP BAR\nLAT PULLDOWN MACHINE\nLEG EXTENSION MACHINE\nLEG CURL MACHINE\nHYPER EXTENSION BENCH\nDIPPING BARS\nABDOMINAL BENCH\n "<<endl;
+system("pause");
+}
+
